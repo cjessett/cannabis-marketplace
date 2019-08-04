add72aaa3c9a60aa86f91c8fa41a98c41a769474
CJ Jessett <cjessett@gmail.com>
2019-08-04 16:52:20 -0700

    Reset scroll on route change

--- src/Router.js
+++ src/Router.js
@@ -6,8 +6,9 @@ import ProductsPage from './Pages/Products';
 import './App.css';
 
 function App() {
+  const resetScroll = () => window.scrollTo(0, 0);
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router onUpdate={resetScroll} basename={process.env.PUBLIC_URL}>
       <Route path='/' exact component={DataCollection} />
       <Route path='/products/' component={ProductsPage} />
     </Router>
