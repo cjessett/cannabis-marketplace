424f6ca0aa42f3d07142798843e3604052dfc3ad
CJ Jessett <cjessett@gmail.com>
2019-08-04 15:35:46 -0700

    Updates

---
+++ precache-manifest.6f8d1cd8c1b9742563ef38b86db1227b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f26bb7faad6898ca296aed51103255c",
+    "url": "/cannabis-marketplace/index.html"
+  },
+  {
+    "revision": "b12415387ad928ba1037",
+    "url": "/cannabis-marketplace/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "95c4d8a269e5b67613e6",
+    "url": "/cannabis-marketplace/static/css/main.8c2c3e82.chunk.css"
+  },
+  {
+    "revision": "b12415387ad928ba1037",
+    "url": "/cannabis-marketplace/static/js/2.252daa2a.chunk.js"
+  },
+  {
+    "revision": "95c4d8a269e5b67613e6",
+    "url": "/cannabis-marketplace/static/js/main.1bfd38e2.chunk.js"
+  },
+  {
+    "revision": "9338a7204e5f44aebcf7",
+    "url": "/cannabis-marketplace/static/js/runtime~main.ac2aa6f1.js"
+  }
+]);
